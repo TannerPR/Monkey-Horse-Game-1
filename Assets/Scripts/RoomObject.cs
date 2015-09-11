d4c01de30e5b184294914c7fb8a1e6cd25bc5c1f
jtabG <jtabgazzola@gmail.com>
2015-09-11 14:36:51 -0400

    Functionality expanded and described with RoomObject.cs

--- Assets/Scripts/RoomObject.cs
+++ Assets/Scripts/RoomObject.cs
@@ -8,29 +8,68 @@ public class RoomObject : MonoBehaviour, IObjectInteraction
     private ItemID m_ItemID = ItemID.UNINITIALIZED;
     public ItemID ItemID { get { return m_ItemID; } }
 
-    private bool m_Visisble = true;
+    private bool m_UsedByPlayer = false;
 
 	// Use this for initialization
 	void Start ()
     {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+        if (m_UsedByPlayer == true) { return; }
 	}
+
+    private void Reset()
+    {
+        m_UsedByPlayer = false;
+        ToggleObject(m_UsedByPlayer);
+    }
 
     private void ActivatedByPlayer()
     {
+        m_UsedByPlayer = true;
+        ToggleObject(m_UsedByPlayer);
+        Debug.Log("now imagine that this is on the player.");
+    }
 
+    private void ToggleObject(bool aCurrentState)
+    {
+        SetObjectVisibility(!aCurrentState);
+        SetObjectCollision(!aCurrentState);
+    }
+
+    private void SetObjectVisibility(bool aState)
+    {
+        if (aState)
+        {
+            // enable any renderer
+        }
+        else
+        {
+            // disable any renderer
+        }
+    }
+
+    private void SetObjectCollision(bool aState)
+    {
+        if (aState)
+        {
+            // enable any collision
+        }
+        else
+        {
+            // disable any collision
+        }
     }
 
     #region INTERFACE
     public void OnInteraction(GameObject aPlayer)
     {
-        Debug.Log("now imagine that this is on the player.");
+        // check if sender is player, if so
+        ActivatedByPlayer();
     }
     #endregion
 }
