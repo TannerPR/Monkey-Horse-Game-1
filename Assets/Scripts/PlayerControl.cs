e85f778d0286ea2ad68da574493373172faac9d0
jtabG <jtabgazzola@gmail.com>
2015-09-11 17:46:37 -0400

    Moved temporary movement control code to PlayerMovement.cs from PlayerControl to keep things compartmentalized Made temporary shirt prefab more accurate size for testing

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,35 +3,17 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField]
-    private CharacterController m_Controller;
 
-    [SerializeField]
-    private float m_MoveSpeed = 2.0f;
-
-    [SerializeField]
-    private float m_TurnSpeed = 2.0f;
 
 	// Use this for initialization
 	void Start () 
     {
-	    if (m_Controller == null)
-        {
-            m_Controller = GetComponent<CharacterController>();
-            if (m_Controller == null)
-            {
-                Debug.Log("Cannot find character contoller");
-                this.enabled = false;
-            }
-        }
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * m_TurnSpeed);
 
-        Vector3 localMoveDir = Input.GetAxis("Vertical") * transform.forward;
-        m_Controller.Move(localMoveDir * Time.deltaTime * m_MoveSpeed);
 	}
 }
