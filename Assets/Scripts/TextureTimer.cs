9dc68e09ffeb1c57ec55c5518a448a0e25f1bf90
Tanner Pye-Richardson <tannerpyerichardson@gmail.com>
2015-09-13 14:36:12 -0400

    + Material scrolling, work in progress

--- Assets/Scripts/TextureTimer.cs
+++ Assets/Scripts/TextureTimer.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TextureTimer : MonoBehaviour {
+public class TextureTimer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_GameObj;
+    private Renderer m_Renderer;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        m_Timer = 0;
+        m_RandomOffset = Vector2.zero;
+        m_Renderer = GetComponent<Renderer>();
+        //m_PropertyName = m_Renderer.material.GetTexture(0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update ()
+    {
+        m_Timer++;
+        if (m_Timer > 45)
+        {
+            m_RandomOffset = new Vector2(Random.Range(0, 20), 
+                                         Random.Range(0, 20));
+            m_Renderer.material.SetTextureOffset("_MainTex", m_RandomOffset);
+            //m_Timer = 0;
+            Debug.Log(m_RandomOffset);
+            Debug.Log(m_Timer);
+        }
 	}
+
+    // private variables
+    float m_Timer;
+    Vector2 m_RandomOffset;
+    string m_PropertyName;
 }
