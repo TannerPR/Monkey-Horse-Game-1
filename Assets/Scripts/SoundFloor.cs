7b522344a651a8423e56e7cccadc8d2767b5a323
James Harper <harp0094@algonquinlive.com>
2015-09-15 05:16:53 -0700

    Commit for justins help
    help

---
+++ Assets/Scripts/SoundFloor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundFloor : MonoBehaviour {
+
+	// Use this for initialization
+    public AudioClip m_Impact;
+    private AudioSource m_AudioSource;
+
+	void Start () {
+        m_AudioSource = GetComponent<AudioSource>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        PlayerControl m_PlayerControl = transform.root.GetComponent<PlayerControl>();
+        if (m_PlayerControl == null)
+        {
+            return;
+        }
+        if (m_AudioSource != null && m_AudioSource.clip != null)
+        {
+            m_AudioSource.PlayOneShot(m_Impact);
+
+        }
+        if (other.transform.root.GetComponent<PlayerControl>())
+        {
+            Debug.Log("PlayerLanded");
+        }
+
+        if (other.transform.tag == "m_TeddyBear")
+        {
+            Debug.Log("Turdylanded");
+        }
+    }
+}
